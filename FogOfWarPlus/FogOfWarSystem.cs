7ad0f95d78ab08da5663849410af6d78a0d826dd
devjarmo <jarmo@netcodez.com>
2019-09-01 12:37:52 -0600

    Good stable point to branch from.

--- FogOfWarPlus/FogOfWarSystem.cs
+++ FogOfWarPlus/FogOfWarSystem.cs
@@ -1,4 +1,5 @@
-﻿using Xenko.Engine;
+﻿using System.Xml;
+using Xenko.Engine;
 using Xenko.Graphics;
 // ReSharper disable UnassignedField.Global
 // ReSharper disable MemberCanBePrivate.Global
@@ -7,8 +8,6 @@ namespace FogOfWarPlus
 {
     public class FogOfWarSystem : StartupScript
     {
-        public Entity FogOfWar;
-
         public override void Start()
         {
             InitializeFogOfWar();
@@ -16,8 +15,14 @@ namespace FogOfWarPlus
 
         private void InitializeFogOfWar()
         {
-            var modelComponent = FogOfWar.Get<ModelComponent>();
+            var modelComponent = Entity.FindChild("FogOfWar").Get<ModelComponent>();
             modelComponent.Enabled = true;
+
+            Entity.FindChild("Orthographic").Get<CameraComponent>().Enabled = true;
+
+            var perspective = Entity.FindChild("Perspective").Get<CameraComponent>();
+            perspective.Enabled = true;
+            perspective.Slot = SceneSystem.GraphicsCompositor.Cameras[0].ToSlotId();
         }
     }
 }
